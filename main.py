bb1c449ef0162b09a0f8b3f8ece8d6fbfa243387
Chris Kahrs <chriskahrs@msn.com>
2022-09-05 15:37:33 -0400

    all

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 import math
 
-flowRate = 5.0 # m3/s
-tankHeight = 5 # m
-tankRadius = 10 # m
-pi = 3.14159 
-tankArea = pi * (tankRadius**2) # pi * r**2
-T = 300 # sec (5 min)
-td = 1 # time differential
-vTank = tankArea * tankHeight
-vLiq = 0
-drainRadius = .5 # m
-drainArea = pi * (drainRadius**2) # pi * r**2
+# Single Tank, everything in meters and seconds 
 
-for i in range(T):
-    vLiq = vLiq + (flowRate * td) 
-    
-    hLiq = max(vLiq / tankArea, 0) # height = v / pi * r**2, can't be below 0
-    
-    vDrain = drainArea * math.sqrt(2 * 9.81 * hLiq) # V = Cd A (2 g H)1/2  ?? Matworks divided by the total tank area?
-    
-    vLiq = max(vLiq - (vDrain / td), 0)
+class Tank():
+    def __init__(self) -> None:
+       
+        self.vFlowRate = 15.0 # m3/s
+        self.tankHeight = 5 # m
+        self.tankRadius = 10 # m
+        self.pi = 3.14159 
+        self.tankArea = self.pi * (self.tankRadius**2) # area = pi * r**2
+        self.td = 0.1 # time differential per loop
+        self.T = math.floor(60 / self.td) # sec (1 min)
+        self.vTank = self.tankArea * self.tankHeight # volume of tank (m3)
+        self.vLiq = 0 # volume of liquid in tank (m3)
+        self.drainRadius = .5 # m
+        self.drainArea = self.pi * (self.drainRadius**2) # area = pi * r**2
+        self.g = 9.81 # m/s2
+        self.hLiq = 0 # m 
+        self.overflowed = False
+        self.emptied = False
+
+    def step(self):
+            
+        vIn = self.vFlowRate * self.td   ## m3 = (m3/s * s)
+        
+        self.vLiq = self.vLiq + vIn # add flowrate to volume of liquid in tank  ## m3 = (m3 + m3)
+        
+        self.hLiq = max(self.vLiq / self.tankArea, 0) # height of Liquid = v / pi * r**2, can't be below 0  ## m = (m3 / pi*m2 )
+        
+        vDrain = self.drainArea * math.sqrt(2 * self.g * self.hLiq) * self.td # V = Cd A (2 g H)1/2 * timeDelta   ## m3 = (m2 * sqrt(m/s2 * m) * s)
+
+        self.vLiq = max(self.vLiq - vDrain, 0) # take away the volume of the drain from the volume of the liquid  ## m3 = (m3 - m3)
+        
+        if self.vLiq > self.vTank:
+           self.overflowed = True
+        elif self.vLiq == 0:
+           self.emptied = True
+
+tank = Tank()
+           
+for i in range(tank.T):
+    tank.step()
         
     if i % 10 == 1:
-        print("current time: " + str(i) + " and height: " + str(vLiq) + " and vDrain: " + str(vDrain))
+        print("current time: " + str(i) + " and height: " + str(tank.hLiq))
 
-    if vLiq > vTank:
+    if tank.overflowed:
         print("overflow!!")
+        break
+    elif tank.emptied:
+        print("emptied!!")
         break
 
--- qued.py
+++ qued.py
@@ -20,35 +20,38 @@ my_button = tk.Button(root, text="my text")
 my_button.pack()
 counter = 0
 
-fig = Figure(figsize=(5, 4), dpi=100)
+fig = Figure(figsize=(10, 4), dpi=100)
 # t = np.arange(0, 3, .01)
 t = [1]
+y = [10]
+u = [2]
+z = [20]
 tm = 1
 xx = 1
 ax = fig.add_subplot()
-y = [10]
+line, = ax.plot(t, y)
+
 ax.set_xlim(0,30)
 ax.set_ylim(0,20)
-line, = ax.plot(t, y)
+line2, = ax.plot(u, z)
 ax.set_xlabel("time [s]")
 ax.set_ylabel("f(t)")
 
-
-my_input = tk.Label(root, text = "start")
+my_input = tk.Button(root, text = "start")
+my_input.pack()
 
 def clicker(event):
+    print("in clicker")
     global counter
     if event.keysym == "Up":
         counter += 1
     if event.keysym == "Down":
         counter -= 1
-    my_input = tk.Label(root, text = "Counter = " + str(counter))
+    my_input.config(text = "Counter = " + str(counter))
     my_input.pack()
-
 
 root.bind("<Key>",clicker)
 my_input.pack(pady=20)
-
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
@@ -63,9 +66,12 @@ def update():
     tm += 1
     t.append(tm)
     line.set_data(t, y)
+    u.append(tm+2)
+    z.append(xx+4)
+    line2.set_data(u,z)
     canvas.draw()
-    my_label.after(100, update)
-
+    root.focus_set()
+    my_label.after(1000, update)
 
 def threader():
     for i in range(10):
@@ -75,20 +81,15 @@ def threader():
 threading.Thread(target=threader).start()
 update()
 
-
-
-
-
 # pack_toolbar=False will make it easier to use a layout manager later on.
 toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
 toolbar.update()
 
-canvas.mpl_connect(
-    "key_press_event", lambda event: print(f"you pressed {event.key}"))
-canvas.mpl_connect("key_press_event", key_press_handler)
+# canvas.mpl_connect(
+#     "key_press_event", lambda event: print(f"you pressed {event.key}"))
+# canvas.mpl_connect("key_press_event", key_press_handler)
 
 button_quit = tk.Button(master=root, text="Quit", command=root.quit)
-
 
 def update_frequency(new_val):
     # retrieve frequency
@@ -100,6 +101,7 @@ def update_frequency(new_val):
     y.append(f)
     
     line.set_data(t, y)
+    line2.set_data(u,z)
 
     # required to update canvas and attached toolbar!
     canvas.draw()
@@ -116,6 +118,9 @@ button_quit.pack(side=tk.BOTTOM)
 slider_update.pack(side=tk.BOTTOM)
 toolbar.pack(side=tk.BOTTOM, fill=tk.X)
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+root.bind("<Key>",clicker)
+root.focus_set()
 
 
 root.mainloop()
